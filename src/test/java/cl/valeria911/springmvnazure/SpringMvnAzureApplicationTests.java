198639673f9da990896b88a24252dc38de275f39
Valeria Gutiérrez <valegutierrezaravena@gmail.com>
2022-03-23 14:11:44 -0300

    add files

---
+++ src/test/java/cl/valeria911/springmvnazure/SpringMvnAzureApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.valeria911.springmvnazure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvnAzureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
