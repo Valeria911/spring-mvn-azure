ad76bdd2b6270eac4bb6919eea60880d6ddbdfca
Valeria Gutiérrez <valegutierrezaravena@gmail.com>
2022-03-24 16:54:26 -0300

    Update message

--- src/main/java/cl/valeria911/springmvnazure/SpringMvnAzureApplication.java
+++ src/main/java/cl/valeria911/springmvnazure/SpringMvnAzureApplication.java
@@ -11,7 +11,7 @@ public class SpringMvnAzureApplication {
 
     @GetMapping("/message")
     public String message(){
-        return "Congrats! your app deployed successfully in Azure!";
+        return "Hello World!";
     }
 
     public static void main(String[] args) {
